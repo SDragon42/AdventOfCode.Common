b747a500a791725550109485531fbcf6e59bf6cd
Nathen Silver <nathen.silver@gmail.com>
2025-01-12 16:55:50 -0900

    Moved Helper methods to the NumbersService

---
+++ tests/Services/NumbersServiceTests/HighestCommonFactorTests.cs
@@ -0,0 +1,40 @@
+﻿namespace AdventOfCode.Common.Tests.Services.NumbersServiceTests;
+
+public class HighestCommonFactorTests : NumbersServiceTestBase
+{
+    [Theory]
+    [InlineData(18, 27, 9)]
+    [InlineData(60, 40, 20)]
+    [InlineData(100, 150, 50)]
+    [InlineData(144, 24, 24)]
+    [InlineData(17, 89, 1)]
+    public void HighestCommonFactor_twoValues(long a, long b, long expected)
+    {
+        var result = _numbersService.HighestCommonFactor(a, b);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(126, 162, 180, 18)]
+    [InlineData(150, 100, 20, 10)]
+    public void HighestCommonFactor_ValidListOfValues(long a, long b, long c, long expected)
+    {
+        var result = _numbersService.HighestCommonFactor([a, b, c]);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [MemberData(nameof(Data_HighestCommonFactor_NullList_ThrowsArgumentException))]
+    public void HighestCommonFactor_NullList_ThrowsArgumentException(IList<long> values)
+    {
+        Assert.Throws<ArgumentException>(() => _numbersService.HighestCommonFactor(values));
+    }
+    public static IEnumerable<object[]> Data_HighestCommonFactor_NullList_ThrowsArgumentException()
+    {
+        yield return new object[] { null! };
+        yield return new object[] { new long[] { } };
+        yield return new object[] { new long[] { 42 } };
+    }
+}
--- tests/Usings.cs
+++ tests/Usings.cs
@@ -2,5 +2,7 @@ global using System;
 global using System.Collections.Generic;
 global using System.Linq;
 global using AdventOfCode.Common.Extensions;
+global using AdventOfCode.Common.Services;
+global using AdventOfCode.Common.Services.Interfaces;
 global using Xunit;
 global using Xunit.Abstractions;
